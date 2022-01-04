b08990713063e182336c8645fd80f850c2e0dcf0
archieyao <168412217@qq.com>
2022-01-04 12:56:19 +0800

    init project

---
+++ hello-world/src/main/java/archieyao/github/io/HelloWorldApp.java
@@ -0,0 +1,12 @@
+package archieyao.github.io;
+
+/**
+ * @author ArchieYao
+ * Created: 2022/1/4 12:49 下午
+ * Description:
+ */
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
